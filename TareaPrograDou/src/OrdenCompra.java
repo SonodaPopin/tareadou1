59aa8ebaf182810bc93add8349839d5cff3f40c5
SonodaPopin <joahernandez2022@udec.cl>
2023-09-29 00:48:23 -0300

    Estrucura del proyecto y el esqueleto de la clase OrdenCompra

---
+++ TareaPrograDou/src/OrdenCompra.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class OrdenCompra {
+    private Date fecha;
+    private String estado;
+
+    public int calcPrecioSinIVA() {
+        return 0;
+    }
+
+    public int calcIVA() {
+        return 0;
+    }
+
+    public int calcPrecio() {
+        return 0;
+    }
+    public int calcPeso(){
+        return 0;
+    }
+}
